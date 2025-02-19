0e0f454d16e94da921f0f87f95658ce3573b4448
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-19 10:30:38 -0300

    Trabalhando com variáveis. Const, let, var

---
+++ modulo01/aula03/app.js
@@ -0,0 +1,19 @@
+// Comentário de uma linha
+
+/*
+    Comentário multilinha
+*/
+let age = 31;
+let currentYear = 2019;
+
+console.log(age, currentYear);
+
+age = 32;
+console.log(age);
+
+const points = 100;
+// points = 101;
+console.log(points);
+
+var score = 75;
+console.log(score);
