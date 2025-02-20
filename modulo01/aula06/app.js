bc1ba169929c861a850feacb37e40fd1e9c0ae16
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-20 14:45:59 -0300

    Trabalhando com Numbers, operadores aritméticos, operadores de incremento e decremento

---
+++ modulo01/aula06/app.js
@@ -0,0 +1,46 @@
+// ? inteiros e decimais
+const radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// ? operadores aritméticos
+// console.log(10 / 2);
+// const reminder = 5 % 2;
+// const reminder = 4 % 2;
+// console.log(reminder);
+
+const area = pi * radius ** 2;
+
+// ? ordem de operações
+const crazyOperation = 5 * (10 - 3) ** 2;
+
+// console.log(area);
+// console.log(crazyOperation);
+
+// ? operadores de incremento e decremento
+
+let postLikes = 10;
+
+// postLikes = postLikes + 1;
+// postLikes++;
+// postLikes--;
+
+// console.log(postLikes);
+
+// ? operadores addition, subtraction, multipication e division
+// postLikes = postLikes + 10;
+// postLikes += 10;
+// postLikes -= 5;
+// postLikes *= 3;
+// postLikes /= 2;
+
+// console.log(postLikes);
+
+// ? NaN - Nota a Number
+// console.log(7 / 'oi'); // NaN
+
+// ? concatenação de string com número
+const likesMessage = 'O post tem ' + postLikes + ' likes.';
+
+console.log(likesMessage);
